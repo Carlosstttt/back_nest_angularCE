c68a7e329d56690ce5aae032aec0bad54a47772e
Carlosstttt <cls.espinales@yavirac.edu.ec>
2026-06-15 08:38:36 -0500

    Agregado módulo de autenticación

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,6 +1,12 @@
+import { IsEmail, IsNotEmpty, Max, MaxLength, Min, MinLength } from 'class-validator';
 import {User} from '../interfaces/user.interface';
 
 export class LoginAuthDto implements User{
+    @IsEmail()
+    @IsNotEmpty()
     email!:string;
+    @MinLength(6)
+    @MaxLength(20)
+    @IsNotEmpty()
     password!:string;
 }
